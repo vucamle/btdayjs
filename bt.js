ccd396443d2a3a4b3a7d63240dc7e32cd7fb906d
vucamle <vuptbd00080@fpt.edu.vn>
2022-11-27 23:18:08 +0700

    index.html

---
+++ bt.js
@@ -0,0 +1,7 @@
+function theky() {
+    let ketQua2 = 2022; // Tạo 1 biến ketqua 2022
+    if(ketQua2 != null) { // Điều kiện
+      console.log("Năm " + ketQua2 + " Thế Kỷ " + Math.ceil(ketQua2 / 100)); // Công Thức math.celi làm tròn đến số nguyên lớn nhất tiếp theo
+    }
+}
+theky() // in ra kết quả
